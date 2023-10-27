803adb745898f3c267257052440d0b949bc76e14
Louis Encinas <enci.louis@gmail.com>
2023-10-27 15:09:50 +0200

    Responsive 2

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # My Website
- 
-# Simple Portfolio Website
 
 This is a simple portfolio website created using plain HTML, CSS, and a bit of JavaScript. The project was primarily developed as a learning experience, and it takes inspiration from the website of Extremely OK Games.
--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+// Couche pour empecher l'interaction avec le fond (pour navbar et modal)
+
+let hide_overlay_modal = document.getElementById("hide_overlay_modal");
+let hide_overlay_navbar = document.getElementById("hide_overlay_navbar");
+
 function hide(div, zInd) {
     div.style.zIndex = zInd;
     div.style.backgroundColor = "rgba(0,0,0,.2)";
@@ -11,10 +16,34 @@ function unhide(div) {
     }, 300);
 }
 
-// Ouverture et fermeture de la fenêtre modal
+// Navbar responsive
 
-let hide_overlay_modal = document.getElementById("hide_overlay_modal");
-let hide_overlay_navbar = document.getElementById("hide_overlay_navbar");
+let dev_btn = document.getElementsByClassName("header_dev_container");
+let close_dev_btn = document.getElementById("navbar_close");
+
+let navbar_dev = document.getElementById("dev_navbar");
+
+function hideNavbar() {
+    navbar_dev.style.right = "-100%";
+    unhide(hide_overlay_navbar);
+    document.body.style.overflow = 'auto';
+}
+
+function unhideNavbar() {
+    navbar_dev.style.right = "0";
+    hide(hide_overlay_navbar, "1");
+}
+
+dev_btn[0].addEventListener("click", unhideNavbar);
+close_dev_btn.addEventListener("click", hideNavbar);
+
+window.addEventListener("resize", function() {
+    if (window.innerWidth > 768) {
+        hideNavbar();
+    }
+});
+
+// Ouverture et fermeture de la fenêtre modal
 
 let modal = document.getElementById("modal_contact");
 
@@ -22,7 +51,7 @@ let open_btn = document.getElementById("header_navbar_link_contact");
 let close_btn = document.getElementById("modal_close");
 
 open_btn.addEventListener("click", function() {
-    modal.style.bottom = "calc(50% - var(--letter_height)/2)";
+    modal.style.bottom = "calc(50% - var(--modal_height)/2)";
     hide(hide_overlay_modal, "9");
 });
 
@@ -49,30 +78,3 @@ if (loc.search("/en/") !== -1) {
     link_en.setAttribute("href", loc);
     link_fr.setAttribute("href", loc.replace("/en/", "/fr/"));
 }
-
-// Menu responsive
-
-let dev_btn = document.getElementsByClassName("header_dev_container");
-let close_dev_btn = document.getElementById("navbar_close");
-
-let navbar_dev = document.getElementById("dev_navbar");
-
-function hideNavbar() {
-    navbar_dev.style.right = "-100%";
-    unhide(hide_overlay_navbar);
-    document.body.style.overflow = 'auto';
-}
-
-function unhideNavbar() {
-    navbar_dev.style.right = "0";
-    hide(hide_overlay_navbar, "1");
-}
-
-dev_btn[0].addEventListener("click", unhideNavbar);
-close_dev_btn.addEventListener("click", hideNavbar);
-
-window.addEventListener("resize", function() {
-    if (window.innerWidth > 768) {
-        hideNavbar();
-    }
-});
